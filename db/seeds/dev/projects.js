7e90eb543e11d5ad0e5f36abb2e2ac2766be4241
Cody-Price <ctprice11@gmail.com>
2019-01-24 14:51:08 -0700

    Add seeding ability for db

--- db/seeds/dev/projects.js
+++ db/seeds/dev/projects.js
@@ -56,11 +56,73 @@ let projectsData = [
         color2: '#777777',
         color3: '#888888',
         color4: '#999999',
-        color5: '#AAAAAA',
-        project_id: project[0]
+        color5: '#AAAAAA'
+      }
+    ]
+  },
+  {
+    name: 'fall',
+    palettes: [
+      {
+        name: 'orange',
+        color1: '#BBBBBB',
+        color2: '#CCCCCC',
+        color3: '#DDDDDD',
+        color4: '#EEEEEE',
+        color5: '#FFFFFF'
+      },
+      {
+        name: 'winter',
+        color1: '#1A1A1A',
+        color2: '#2B2B2B',
+        color3: '#3C3C3C',
+        color4: '#4D4D4D',
+        color5: '#5E5E5E'
       }
     ]
   }
 ]
 
-exports.seed
+const createProject = (knex, project) => {
+  return knex('projects').insert({
+    name: project.name
+  }, 'id')
+    .then(projectId => {
+      let palettePromises = [];
+
+      project.palettes.forEach(palette => {
+        palettePromises.push(
+          createPalette(knex, {
+            name: palette.name,
+            color1: palette.color1,
+            color2: palette.color2,
+            color3: palette.color3,
+            color4: palette.color4,
+            color5: palette.color5,
+            project_id: projectId[0]
+          }, 'id')
+        )
+      });
+
+      return Promise.all(palettePromises);
+    })
+}
+
+const createPalette = (knex, palette) => {
+  return knex('palettes').insert(palette);
+}
+
+exports.seed = (knex, Promise) => {
+  return knex('palettes').del()
+    .then(() => knex('projects').del())
+    .then(() => {
+      let projectPromises = [];
+
+      projectsData.forEach(project => {
+        projectPromises.push(createProject(knex, project));
+      });
+
+      return Promise.all(projectPromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
