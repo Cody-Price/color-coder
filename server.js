f8508a938c7145a9adfe26dd98641b1efdf7f01e
Cody-Price <ctprice11@gmail.com>
2019-01-27 20:18:39 -0700

    Fix small syntax issues

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const environment = process.env.NODE_ENV || 'development';
-//set environment to be either the node environment or development
 const configuration = require('./knexfile.js')[environment];
-//setting the configuration of the database based on the current environment
 const database = require('knex')(configuration);
-//applies the correct configuration to the database
 const express = require('express');
-//imports express and assigns it to the variable 'express'
 const app = express();
-//sets up our app using the express function
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'COLOR CODER';
 
@@ -26,19 +21,19 @@ app.get('/api/v1/palettes', (request, response) => {
 });
 
 app.get('/api/v1/palettes/:id', (request, response) => {
-  const { id } = request.params
+  const { id } = request.params;
 
   database('palettes').select()
     .then(palettes => {
-      let foundPalette = palettes.find(palette => palette.id === parseInt(id))
+      let foundPalette = palettes.find(palette => palette.id === parseInt(id));
       if(foundPalette) {
-        response.status(200).json(foundPalette)
+        response.status(200).json(foundPalette);
       } else {
-        response.sendStatus(404)
-      }
+        response.sendStatus(404);
+      };
     })
     .catch(error => {
-      response.send(500).json({ error })
+      response.send(500).json({ error });
     });
   });
   
@@ -50,17 +45,17 @@ app.post('/api/v1/palettes', (request, response) => {
       return response
       .status(422)
       .send({ error: `Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String>, project_id: <Number> }. You're missing a "${requiredParameter}" property.` })
-    }
-  }
+    };
+  };
   
   database('palettes').insert(palette, 'id')
   .then(palette => {
-    response.status(201).json({ id: palette[0] })
+    response.status(201).json({ id: palette[0] });
   })
   .catch(error => {
     response.status(500).json({ error })
   });
-})
+});
 
 
 app.post('/api/v1/projects', (request, response) => {
@@ -68,43 +63,43 @@ app.post('/api/v1/projects', (request, response) => {
   if (project.name) {
     database('projects').select('name')
       .then(projectNames => {
-        const names = projectNames.map(project => project.name.toLowerCase())
+        const names = projectNames.map(project => project.name.toLowerCase());
         if (names.includes(project.name.toLowerCase())) {
           response.status(409).json({ error: 'Project name already exists, please pick a different project name.' });
         } else {
           database('projects').insert(project, 'id')
-            .then(project => response.status(201).json({ id: project[0] }))
-        }
+            .then(project => response.status(201).json({ id: project[0] }));
+        };
       })
       .catch(error => response.status(500).json({error}));
   } else {
     response.status(422).json({ error: `Expected format: { name: <String> }. You're missing a "name" property.`});
-  }
+  };
 });
 
 app.delete('/api/v1/palettes/:id', (request, response) => {
   database('palettes').where('id', request.params.id).delete()
     .then(palette => {
-      response.status(202).json({ id: palette[0] })
+      response.status(202).json({ id: palette[0] });
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
     .then((projects) => {
-      response.status(200).json(projects)
+      response.status(200).json(projects);
     })
     .catch((error) => {
       response.status(500).json({error});
     });
-})
+});
 
 app.get('/api/v1/projects/:id', (request, response) => {
-  const { id } = request.params
+  const { id } = request.params;
   database('projects').select()
     .then((projects) => {
       let foundProject = projects.find(project => project.id === parseInt(id));
@@ -112,30 +107,30 @@ app.get('/api/v1/projects/:id', (request, response) => {
         response.status(200).json(foundProject);
       } else {
         response.sendStatus(404);
-      }
+      };
     })
     .catch(error => {
       response.status(500).json({error})
     });
-})
+});
 
 
 app.delete('/api/v1/projects/:id', (request, response) => {
   database('palettes').where('project_id', request.params.id).delete()
     .then(palette => {
-      response.status(202).json({  project_id: palette[0] })
+      response.status(202).json({  project_id: palette[0] });
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
+      response.status(500).json({ error });
+    });
   database('projects').where('id', request.params.id).delete()
     .then(project => {
-      response.status(202).json({ id: project[0] })
+      response.status(202).json({ id: project[0] });
     })
     .catch(error => {
-      response.status(500).json({ error })
-    })
-})
+      response.status(500).json({ error });
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
